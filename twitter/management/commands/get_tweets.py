48aeb3969b835f5fec256d8c9a2eb2327dae12e0
Jon Sustar <Jon@Jon-Sustars-MacBook-Pro.local>
2010-12-12 13:42:34 -0500

    Lots of changes

--- twitter/management/commands/get_tweets.py
+++ twitter/management/commands/get_tweets.py
@@ -14,36 +14,7 @@ class Command(NoArgsCommand):
             
         print "TRY AUTHORIZATION ---------"
         '''
-        consumer_token = "KALkUmw13ZaXdsXHOT5SQ"
-        consumer_secret = "MtNSzay6FvDxK5lKbcM5gDjF9Jz3U5g7ZURyGBNkWFM"
-        
-        auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
-        
-        '''
-        try:
-            redirect_url = auth.get_authorization_url()
-            print redirect_url
-        except tweepy.TweepError:
-            print 'Error! Failed to get request token.'
-            
-        verifier = raw_input('Verifier:')
-        
-        try:
-            auth.get_access_token(verifier)
-            print "SUCCESS"
-            print "KEY: " + auth.access_token.key
-            print "SECRET: " + auth.access_token.secret
-        except tweepy.TweepError:
-            print 'Error! Failed to get access token.'
-            
-        # for conciseadvice:
-        # key: 80658421-fIMM55xHQPHNvf8N0AmEOoMWntFwQgB97adSIjtja
-        # secret: bC4H3BW2luMwDbQGiqmpGnWXiCuv9HihxTl1fKEgmjs
-        
-        '''
-        
-        key = "80658421-fIMM55xHQPHNvf8N0AmEOoMWntFwQgB97adSIjtja"
-        secret = "bC4H3BW2luMwDbQGiqmpGnWXiCuv9HihxTl1fKEgmjs"
+
         
         auth.set_access_token(key, secret)
         api = tweepy.API(auth)
